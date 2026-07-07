8b3f59b05f5058ba6d9a3102fa1ecf3341f9ea27
Dinushka Sandeepa <161791095+Dinushka20@users.noreply.github.com>
2026-07-07 17:50:48 +0530

    chore: update backend API URL

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // Add https:// prefix
-    baseURL: "https://testpilot-api-haf3hkfscvc3frg4.southeastasia-01.azurewebsites.net"
+    baseURL: "https://testpilot-apit-geagb9dqgpbpcthx.southeastasia-01.azurewebsites.net"
 });
 
 export default api;
